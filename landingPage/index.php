7ce23edb8832564177697864ad43d5d39a44a02a
Doreen Tiema <doreentiema87@gmail.com>
2024-03-22 18:15:09 +0300

    linking of the login.php file inside the index.php file

--- landingPage/index.php
+++ landingPage/index.php
@@ -61,7 +61,7 @@
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><span class="glyphicon glyphicon-log-in"></span> Login</a>
                       <ul class="dropdown-menu drop_1 " aria-labelledby="navbarDropdown">
-                          <li><a class="dropdown-item" href="../pas-login.php">Passenger</a></li>
+                          <li><a class="dropdown-item" href="pas-login.php">Passenger</a></li>
                           <li><a class="dropdown-item" href="../admin-login.php">Admin</a></li>
                       </ul>
                     </li>
